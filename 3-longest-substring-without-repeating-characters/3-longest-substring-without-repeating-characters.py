5d7e224a27577c004b4df8de205f9f4a02c1d140
Nguyễn Mạnh Toàn <79891657+Kin2k1@users.noreply.github.com>
2022-01-07 14:32:15 +0700

    Time: 1834 ms (5.00%), Space: 14.2 MB (93.49%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,17 +5,15 @@ class Solution:
         for i in range(len(s) -1):
             #length = 1
             for j in range(i +1, len(s)):
-                if s[i: j].find(s[j]) == -1:
-                    print()
-                else:
+                if s[i: j].find(s[j]) != -1:
                     if total < len(s[i: j]):
                         total = len(s[i: j])
-                    print("s", s[i: j])
+                    #print("s", s[i: j])
                     break
                 if j == len(s) -1:
                     if total < len(s[i: j+1]):
                         total = len(s[i: j+1])
-                    print("s", s[i: j+1])
+                    #print("s", s[i: j+1])
             
         return total
                 
