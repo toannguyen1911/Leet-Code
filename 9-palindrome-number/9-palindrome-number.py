138b14dd1eb36a27af856484fbe69a87dfa2eb6b
Nguyễn Mạnh Toàn <79891657+Kin2k1@users.noreply.github.com>
2022-01-09 16:59:43 +0700

    Time: 104 ms (14.82%), Space: 14.1 MB (92.44%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,8 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = list(str(x))
-        a = x.copy()
-        a.reverse()
-        print(x, a)
-        return x == a
+        return str(x) == str(x)[::-1]
         
