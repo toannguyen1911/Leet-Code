361dfc3b86346cf3db22caf64194ba1a63eae93b
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-01-05 11:48:08 +0700

    Time: 36 ms (79.50%), Space: 17.2 MB (15.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,23 +1,10 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        temp = 0;
-        
-        if (len(digits) == 1):
-            if (digits[0] == 9):
-                return [1, 0];
-            return [digits[0] +1];
-        
+    def plusOne(self, digits: List[int]) -> List[int]:        
         for index in range(len(digits), 0, -1):
             if (digits[index -1] < 9):
-                temp = 0; 
                 digits[index -1] += 1;
-                break;
+                
+                return digits;
+            digits[index -1] = 0;
             
-            if (digits[index -1] == 9):
-                digits[index -1] = 0;
-                temp = 1;
-            
-        if (temp > 0):
-            digits[0] = 0;
-            return [1] + digits;
-        return digits
+        return [1] + digits;
