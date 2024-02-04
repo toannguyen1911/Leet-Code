56b1a830a2dcf316bcc32d2262de7d67594f52b4
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-02-04 16:18:30 +0700

    Time: 67 ms (61.57%), Space: 23.4 MB (54.22%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -6,8 +6,23 @@ class Solution:
                    
     def countBits(self, n: int) -> List[int]:
         result = [];
+        memory = {};
+        
         for index in range(n + 1):
-            result.append(self.toBinary(index));
+            count = 0;
+            x = index;
+            
+            while (x > 0):
+                if (x in memory):
+                    count += memory[x];
+                    break;
+                count += x % 2;
+                x = x //2;
+                
+            if (index not in memory):
+                memory[index] = count;
+            result.append(count);
+        
         return result;
         
         
