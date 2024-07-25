88d36c6687e3dbf80c46f3f39ad719ca89c06f00
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-07-25 22:24:32 +0700

    Time: 973 ms (75.51%), Space: 25.3 MB (36.95%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mergeArray(self,l_array, r_array):
+          l = 0
+          r = 0
+          result = []
+          while (1):
+            if l_array[l] < r_array[r]:
+              result.append(l_array[l])
+              if (l + 1 >= len(l_array)):
+                return result + r_array[r:]
+              l += 1
+              continue
+
+            result.append(r_array[r])
+            if (r + 1 >= len(r_array)):
+              return result + l_array[l:]
+            r += 1
+
+    def mergeSort(self,nums, left, right):
+      if (left == right):
+        return [nums[left]]
+
+      center = (right + left ) // 2
+      l_array = self.mergeSort(nums, left, center)
+      r_array = self.mergeSort(nums, center + 1, right)
+
+      return self.mergeArray(l_array, r_array)
+    
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.mergeSort(nums, 0, len(nums) -1)
+        
+        
