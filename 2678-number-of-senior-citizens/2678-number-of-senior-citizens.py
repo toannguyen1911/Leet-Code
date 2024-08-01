31126b257ef38afbeb48e15753ca504a90c46c6b
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-08-01 22:13:24 +0700

    Time: 56 ms (6.50%), Space: 16.6 MB (32.17%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,10 +1,17 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
+        '''
         result = 0
         
         for detail in details:
             if (int(detail[11: 13]) > 60):
                 result += 1;
-                
+               
         return result
+        '''
+        # code optimization
+        return sum(1 if int(detail[11:13]) > 60 else 0 for detail in details)
+    
+        
+        
         
