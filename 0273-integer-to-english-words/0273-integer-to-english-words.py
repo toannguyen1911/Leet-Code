9f309c6bc8cec84b26bffaecd3a7916dd863f0ac
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-08-07 21:53:36 +0700

    Time: 32 ms (83.30%), Space: 16.5 MB (75.27%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def numberToWords(self, num):
+        def to_words(n):
+            if n < 20:
+                return less_than_twenty[n]
+            elif n < 100:
+                return tens[n // 10] + ('' if n % 10 == 0 else ' ' + less_than_twenty[n % 10])
+            else:
+                return less_than_twenty[n // 100] + ' Hundred' + ('' if n % 100 == 0 else ' ' + to_words(n % 100))
+            
+        if num == 0:
+            return "Zero"
+        
+        result = []
+        tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        less_than_twenty = [
+            "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", 
+            "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", 
+            "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        ]
+        
+
+        billions, num = divmod(num, 1000000000)
+        millions, num = divmod(num, 1000000)
+        thousands, num = divmod(num, 1000)
+        
+        if billions:
+            result.append(to_words(billions) + ' Billion')
+        if millions:
+            result.append(to_words(millions) + ' Million')
+        if thousands:
+            result.append(to_words(thousands) + ' Thousand')
+        if num:
+            result.append(to_words(num))
+        
+        return ' '.join(result)
