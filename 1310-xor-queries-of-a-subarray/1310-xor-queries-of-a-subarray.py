be7f8c33421944f0e9a5aa6194f3b35d90a8e45e
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-09-13 22:12:09 +0700

    Time: 6281 ms (5.06%), Space: 31.2 MB (81.84%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -4,14 +4,17 @@ class Solution:
         result = [];
         for query in queries:
             key = str(query[0]) + '-' + str(query[1]);
+            # Check key in memories
             if (key in memories.keys()):
                 result.append(memories[key]);
                 continue;
+                
             flag = arr[query[0]];
             for index in range (query[0] + 1, query[1] +1):
-                # print(flag, index)
                 flag ^= arr[index]
+            
+            # Save pair value to memories
             memories[key] = flag;
             result.append(flag)
-        # print(memories);
+
         return result;
