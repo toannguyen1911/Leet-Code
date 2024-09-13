2fc690b9c26899fadc9a10d5a828f6a9b1e31dc3
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2024-09-13 22:29:10 +0700

    Time: 306 ms (49.06%), Space: 31.5 MB (10.11%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,20 +1,16 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        memories = {}
-        result = [];
-        for query in queries:
-            key = str(query[0]) + '-' + str(query[1]);
-            # Check key in memories
-            if (key in memories.keys()):
-                result.append(memories[key]);
-                continue;
-                
-            flag = arr[query[0]];
-            for index in range (query[0] + 1, query[1] +1):
-                flag ^= arr[index]
-            
-            # Save pair value to memories
-            memories[key] = flag;
-            result.append(flag)
-
-        return result;
+        answers = []
+        memory = [arr[0]]
+        
+        # Calculate xor from 0 to index and save to memory
+        for index in range (1, len(arr)):
+            memory.append(memory[index -1] ^ arr[index]);
+        
+        # Caluculate the query
+        for left, right in queries:
+            # if left is 0, the answer is simple an xnor from 0 to right. (memory[right]).
+            # otherwise we need to exclude the part of the array before left.
+            answer = memory[right] if left == 0 else memory[right] ^ memory[left -1]
+            answers.append(answer)
+        return answers;
