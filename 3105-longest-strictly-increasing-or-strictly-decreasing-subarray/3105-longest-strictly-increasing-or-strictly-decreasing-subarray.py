eab0eba0a644d450f935eb035587295b06aa9671
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-02-03 21:11:42 +0700

    Time: 30 ms (5.83%), Space: 18 MB (11.65%) - LeetHub

---
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        ans = 1;
+        count = 1;
+        n = len(nums);
+
+        for i in range (n -1):
+            # update ans
+            if (count > ans):
+                print('update count: ' + str(count))
+                ans = count;
+            # fast stop
+            if (ans >= n - i):
+                return ans;
+            
+            # reset loop variable
+            op = None;
+            root = nums[i];
+            count = 1;
+            for j in range (i +1, len(nums)):
+                if root == nums[j]:
+                    break;
+                print(i, j, op, root, count, ans)
+                # update operator
+                if (op == None):
+                    op = 1 if root > nums[j] else -1;
+                    root = nums[j]
+                    count += 1;
+                    continue;
+                #
+                compare = (root - nums[j]) * op;
+                print('compare' + str(compare))
+                if (compare <= 0):
+                    break;
+                # update  sub array
+                count += 1;
+                root = nums[j];
+               
+        return count;
