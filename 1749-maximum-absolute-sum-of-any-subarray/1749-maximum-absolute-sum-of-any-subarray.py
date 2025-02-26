fc8e2b207173ed635b9bf3d08ac3c85a7a8940a3
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-02-26 18:25:58 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        # Kadane’s algorithm 
+        # for each nums: 
+        # choice 1: sub array + current num; 
+        # choic2 2: start new sub array
+        # maxEnding at index i = max(maxEnding at index (i – 1) + arr[i], arr[i]) 
+        max_ending_here = nums[0];
+        min_ending_here = nums[0];
+        max_so_far = nums[0];
+        min_so_far = nums[0];
+
+        for i in range(1, len(nums)):
+            max_ending = max(nums[i], max_ending + nums[i]);
+            min_ending = min(nums[i], min_ending + nums[i]);
+            max_so_far = max(max_so_far, max_ending);
+            min_so_far = min(min_so_far, min_ending);
+
+        return max(abs(max_so_far), abs(min_so_far));
