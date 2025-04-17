ca56581c62aa7f55e7d3c2646d325fd138d392a0
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-04-17 18:00:50 +0700

    Time: 7 ms (95%), Space: 18 MB (12.5%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,9 +1,34 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        n = len(nums);
-        ans = 0;
-        for i in range (n -1):
-            for j in range (i +1, n):
-                if (nums[i] == nums[j] and ((i * j) % k == 0)):
-                    ans += 1;
-        return ans;
+
+        def bruteForce():
+            n = len(nums);
+            ans = 0;
+            for i in range (n -1):
+                for j in range (i +1, n):
+                    if (nums[i] == nums[j] and ((i * j) % k == 0)):
+                        ans += 1;
+            return ans;
+
+        def optimized():
+            '''
+                We just need to check for the same numbers.
+                So group them together
+            '''
+            mem = defaultdict(list);
+            ans = 0;
+
+            # Step 1: Group Genins (numbers) by village (same value)
+            for i, val in enumerate(nums):
+                # Step 2: Check only within the same group (same number)
+                for j in mem[val]:
+                    if (i * j) % k == 0:
+                        ans += 1;
+                # Step 3: Add current index to the list of indices for the number
+                mem[val].append(i);
+            
+            return ans;
+
+        # return bruteForce();
+        return optimized();
+        
