8808c8c2631aa4857b59df21bc749996fd16dc7d
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-05-13 22:56:11 +0700

    Time: 382 ms (87.58%), Space: 22.4 MB (24.18%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
@@ -7,14 +7,51 @@ class Solution:
         return length of s
     """
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        ans = len(s);
-        mod = 10**9 + 7;
-        # convert to ascii
-        # a = 97, z = 122
-        for c in s:
-            c = ord(c);
-            ans += (c + t -1) // 122;
-            ans %= mod;
+        def bruteForce(s, t):
+            dp = [0] * 26;
+            ans = 0;
+            mod = 10**9 + 7;
 
-        return ans;
+            for c in s:
+                dp[ord(c) -97] += 1;
+            
+            for i in range(t):
+                cur = [0] * 26;
+
+                # check z
+                if dp[25]:
+                    cur[0] = (cur[0] + dp[25]) % mod;
+                    cur[1] = (cur[1] + dp[25]) % mod;
+                for j in range(25):
+                    num = dp[j]
+                    if (num):
+                        cur[j +1] = (cur[j + 1] + num) % mod;
+                dp = cur
+            for m in dp:
+                ans = (ans + m) % mod;
+            return ans;
+
+        def dynamicProgram(s, t):
+            ans = 0;
+            mod = 10**9 + 7;
+            dp = [0] * (t + 26);
+
+            # Start with 'a'
+            # dp[i]: 
+            #   i < 26: dp[i] = 1; <=> next character
+            #   i >= 26: dp[i] = dp[i -26] + dp[i -25] <=> a, b
+            
+            for i in range(26): 
+                dp[i] = 1;
+            for i in range(26, t + 26):
+                dp[i] = (dp[i - 26] + dp[i - 25]) % mod;
+            
+            for ch in s:
+                dp_result = dp[ord(ch) - ord('a') + t];
+                ans = (ans + dp_result) % mod;
+            
+            return ans;
+        
+        # return bruteForce(s, t);
+        return dynamicProgram(s, t);
         
