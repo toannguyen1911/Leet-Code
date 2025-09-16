ea9d9ad0976d49e26dca8f42c47cdddd1beb7d55
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-09-16 22:17:58 +0700

    Time: 137 ms (51.09%), Space: 31.6 MB (59.78%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def replaceNonCoprimes(self, nums):
+        stack = [];
+        
+        for num in nums:
+            while stack and math.gcd(stack[-1], num) > 1:
+                # If the top of the stack and current number are non-coprime, 
+                # replace them with their LCM
+                top = stack.pop();
+                num = (top * num) // math.gcd(top, num);
+            
+            # Add the current number (or merged LCM) to the stack
+            stack.append(num);
+        
+        # Return the final array after processing all numbers
+        return stack;
