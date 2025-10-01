5106935920109b7aaedb7f73c60f985120d23d30
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-10-01 22:23:02 +0700

    Time: 11 ms (82.9%), Space: 18.8 MB (45.71%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -5,6 +5,6 @@ class Solution:
         nums.sort(reverse = True);
 
         for i in range(n -2):
-            if nums[i] < nums[i +1] + nums[i + 2];
+            if nums[i] < nums[i +1] + nums[i + 2]:
                 return nums[i] + nums[i +1] + nums[i +2];
         return ans;
