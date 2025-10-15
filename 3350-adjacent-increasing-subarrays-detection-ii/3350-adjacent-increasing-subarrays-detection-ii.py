504c9b5678792b0be51ce75eba5eee2342cfc581
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-10-15 23:02:21 +0700

    Time: 1650 ms (14.19%), Space: 45.9 MB (39.35%) - LeetHub

---
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums);
+        pre, suff = [0] *(n + 1), [0] *(n+1);
+        res = 0;
+
+        # pre[i] = Length of increasing subarray ending at i - 1.
+        for i in range(1, n + 1):
+            pre[i] = pre[i-1] + 1 if i > 1 and nums[i-2] < nums[i-1] else 1;
+        # suff[i] = Length of increasing subarray starting at i - 1.
+        for i in range(n, 0, -1):
+            suff[i] = suff[i+1] + 1 if i < n and nums[i-1] < nums[i] else 1;
+        
+        for i in range(1, n + 1):
+            res = max(res, min(pre[i-1], suff[i]));
+        return res;
