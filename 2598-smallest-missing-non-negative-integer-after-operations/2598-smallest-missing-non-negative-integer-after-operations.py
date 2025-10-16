b103d4f8058bc05671b4853ee2bace471ef6f4c4
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-10-16 21:16:59 +0700

    Time: 102 ms (67.04%), Space: 38.4 MB (58.49%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
@@ -1,16 +1,19 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        mex = float('inf');
-        pos = 0;
-
         freq = defaultdict(int);
         for num in nums:
             freq[num % value] += 1;
 
+        min_freq = float('inf');
+        remainder = 0;
         for i in range(value):
             if freq[i] == 0:
+                # If this remainder never appears,
+                # the smallest missing number is exactly i
                 return i;
-            if freq[i] < mex:
-                mex = freq[i];
-                pos = i;
-        return (value *mex) + pos;
+
+            # Find the remainder that appears least often.
+            if freq[i] < min_freq:
+                min_freq = freq[i];
+                remainder = i;
+        return (value * min_freq) + remainder;
