e3d833465ca8a3feb8b4c336628acbdf60f9d6d8
Nguyễn Mạnh Toàn <79891657+toannguyen1911@users.noreply.github.com>
2025-11-03 22:12:46 +0700

    Time: 144 ms (31.43%), Space: 24.3 MB (95.68%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -5,19 +5,8 @@ class Solution:
         cost = neededTime[0];
         count = 1;
 
-        for i in range(n):
-            if i == n -1:
-                if count > 1:
-                    res += cost;
-                break;
-            if (colors[i] == colors[i +1]):
-                count += 1;
-                if neededTime[i +1] < cost:
-                    cost = neededTime[i +1];
-            else:
-                if count > 1:
-                    res += cost;
-                cost = neededTime[i +1];
-                count = 1;
-        
+        for i in range(1 , n):
+            if (colors[i] == colors[i -1]):
+                res += min(neededTime[i], neededTime[i -1]);
+                neededTime[i] = max(neededTime[i], neededTime[i -1]);
         return res;
